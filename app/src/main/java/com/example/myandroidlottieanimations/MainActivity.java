1fdfc576bd4a7a99fc1305e5306a384a60e0b7a7
DarushDev <darushfaqiri@gmail.com>
2018-09-13 15:16:51 +0800

    Pause and resume animations using buttons

--- app/src/main/java/com/example/myandroidlottieanimations/MainActivity.java
+++ app/src/main/java/com/example/myandroidlottieanimations/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myandroidlottieanimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -9,18 +10,47 @@ import static com.airbnb.lottie.LottieDrawable.INFINITE;
 
 public class MainActivity extends AppCompatActivity {
 
-    LottieAnimationView lottieAnimationView;
+    LottieAnimationView lottieConfetti;
+    LottieAnimationView lottieFavorite;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lottieAnimationView = findViewById(R.id.lottie_from_code);
-        lottieAnimationView.setAnimation("favourite_icon.json");
-        lottieAnimationView.playAnimation();
-        lottieAnimationView.setRepeatCount(INFINITE);
+        lottieFavorite = findViewById(R.id.lottie_anim_favourite);
 
+        lottieConfetti = findViewById(R.id.lottie_anim_confetti);
+        lottieConfetti.setAnimation("confetti.json");
+        lottieConfetti.playAnimation();
+        lottieConfetti.setRepeatCount(INFINITE);
+
+        findViewById(R.id.btn_main_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lottieConfetti.isAnimating()) {
+                    lottieConfetti.pauseAnimation();
+                }
+
+                if (lottieFavorite.isAnimating()) {
+                    lottieFavorite.pauseAnimation();
+                }
+            }
+        });
+
+        findViewById(R.id.btn_main_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!lottieConfetti.isAnimating()) {
+                    lottieConfetti.resumeAnimation();
+                }
+
+                if (!lottieFavorite.isAnimating()) {
+                    lottieFavorite.resumeAnimation();
+                }
+            }
+        });
 
     }
 }
