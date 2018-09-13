666304c553a117ca9c336bff55ae523612869e49
DarushDev <darushfaqiri@gmail.com>
2018-09-13 13:55:04 +0800

    Play lottie view from code

--- app/src/main/java/com/example/myandroidlottieanimations/MainActivity.java
+++ app/src/main/java/com/example/myandroidlottieanimations/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.myandroidlottieanimations;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import static com.airbnb.lottie.LottieDrawable.INFINITE;
+
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lottieAnimationView = findViewById(R.id.lottie_from_code);
+        lottieAnimationView.setAnimation("favourite_icon.json");
+        lottieAnimationView.playAnimation();
+        lottieAnimationView.setRepeatCount(INFINITE);
+
+
     }
 }
